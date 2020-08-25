a4b1cba3fd2e88486a3324f3dbd89201da7e65fa
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-25 12:38:15 -0500

    thirdd practice question

--- strings_week1.rb
+++ strings_week1.rb
@@ -1,9 +1,17 @@
-# first_name = "Taylor"
-# last_name = "Dorsett"
+# # first_name = "Taylor"
+# # last_name = "Dorsett"
 
-# puts "Hello " + first_name + " " + last_name + "!"
+# # puts "Hello " + first_name + " " + last_name + "!"
 
-first_name = "Todd"
-last_name = "Doger"
+# first_name = "Todd"
+# last_name = "Doger"
 
-puts "Hello #{first_name} #{last_name}!"
+# puts "Hello #{first_name} #{last_name}!"
+
+puts "Please enter a word!"
+
+input = gets.chomp
+
+if input.upcase == "MARCO"
+  puts "Polo"
+end
