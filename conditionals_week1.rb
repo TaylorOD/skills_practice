f2c15e3496d609f19e0ab5768bb377b71d60a2ff
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-25 09:41:52 -0500

    change conditional

--- conditionals_week1.rb
+++ conditionals_week1.rb
@@ -1,7 +1,17 @@
-var = 0
+# var = 0
 
-if var == 10
-  puts "0"
-else
+# if var == 10
+#   puts "0"
+# else
+#   puts "-1"
+# end
+
+var = 1
+
+if var < 10
   puts "-1"
+elsif var > 10
+  puts "0"
+elsif var == 10
+  puts "0"
 end
