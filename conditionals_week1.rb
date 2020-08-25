16dd2785632570473c42bb97e85a0ba4c6dd89b8
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-25 09:48:04 -0500

    change conditions add elsifs

--- conditionals_week1.rb
+++ conditionals_week1.rb
@@ -1,34 +1,46 @@
-# var = 0
+# # var = 0
 
-# if var == 10
-#   puts "0"
+# # if var == 10
+# #   puts "0"
+# # else
+# #   puts "-1"
+# # end
+
+# # var = 1
+
+# # if var < 10
+# #   puts "-1"
+# # elsif var > 10
+# #   puts "0"
+# # elsif var == 10
+# #   puts "0"
+# # end
+
+# # var = 1
+# # var_2 = 2
+
+# # if var < 10 || var_2 < 10
+# #   puts "1"
+# # else
+# #   puts "0"
+# # end
+
+# var = 150
+
+# if var > 9000
+#   puts 1
 # else
-#   puts "-1"
+#   puts -1
 # end
 
-# var = 1
+var = 28
 
-# if var < 10
-#   puts "-1"
-# elsif var > 10
-#   puts "0"
-# elsif var == 10
-#   puts "0"
-# end
-
-# var = 1
-# var_2 = 2
-
-# if var < 10 || var_2 < 10
-#   puts "1"
-# else
-#   puts "0"
-# end
-
-var = 150
-
-if var > 9000
-  puts 1
+if var < 10
+  puts 9
+elsif var < 20
+  puts 19
+elsif var < 30
+  puts 29
 else
   puts -1
 end
