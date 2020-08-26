231aa83c724ea447db02ac7447c2a6707ee690fe
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-26 09:27:37 -0500

    2nd function added

--- week1/functions_practice.rb
+++ week1/functions_practice.rb
@@ -1,5 +1,11 @@
-def times_two(number)
-  return number * 2
+# def times_two(number)
+#   return number * 2
+# end
+
+# puts times_two(8)
+
+def all_cap(string)
+  return string.upcase
 end
 
-puts times_two(8)
+puts all_cap("hello")
