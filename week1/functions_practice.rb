c30bc50f053461b779653fca067e6872937200f4
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-26 09:29:15 -0500

    add 3rd function

--- week1/functions_practice.rb
+++ week1/functions_practice.rb
@@ -4,8 +4,14 @@
 
 # puts times_two(8)
 
-def all_cap(string)
-  return string.upcase
+# def all_cap(string)
+#   return string.upcase
+# end
+
+# puts all_cap("hello")
+
+def sub1_2(number1, number2)
+  return number1 - number2
 end
 
-puts all_cap("hello")
+puts sub1_2(10, 11)
