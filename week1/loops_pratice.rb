9fe24bdd121761033ae243c08a87145472a35c13
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-27 12:08:04 -0500

    add 7th loop practice

--- week1/loops_pratice.rb
+++ week1/loops_pratice.rb
@@ -1,43 +1,50 @@
-# # # # index = 1
+# # # # # # index = 1
 
-# # # # while index <= 10
-# # # #   p index
-# # # #   index += 1
+# # # # # # while index <= 10
+# # # # # #   p index
+# # # # # #   index += 1
+# # # # # # end
+
+# # # # # index = 1
+
+# # # # # while index <= 5
+# # # # #   puts "hello"
+# # # # #   index += 1
+# # # # # end
+
+# # # # # while true
+# # # # #   puts "Please input a word:"
+# # # # #   input = gets.chomp
+# # # # #   if input.downcase == "stop"
+# # # # #     break
+# # # # #   end
+# # # # # end
+
+# # # # i = 5
+
+# # # # while i <= 100
+# # # #   puts i
+# # # #   i += 5
 # # # # end
 
-# # # index = 1
+# # # i = 0
 
-# # # while index <= 5
-# # #   puts "hello"
-# # #   index += 1
+# # # while i < 5
+# # #   puts 9000
+# # #   i += 1
 # # # end
 
-# # # while true
-# # #   puts "Please input a word:"
-# # #   input = gets.chomp
-# # #   if input.downcase == "stop"
-# # #     break
-# # #   end
-# # # end
-
-# # i = 5
-
-# # while i <= 100
-# #   puts i
-# #   i += 5
+# # while true
+# #   puts "Please input a number:"
+# #   input = gets.chomp
+# #   if input.to_i > 10
+# #     break
+# #   end
 # # end
 
-# i = 0
+# i = 50
 
-# while i < 5
-#   puts 9000
+# while i <= 70
+#   puts i
 #   i += 1
 # end
-
-while true
-  puts "Please input a number:"
-  input = gets.chomp
-  if input.to_i > 10
-    break
-  end
-end
