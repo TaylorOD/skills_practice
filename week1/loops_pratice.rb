3c7ac0475b138d6f84586909362b3bb9defa879e
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-27 12:00:54 -0500

    add 3rd practice loop

--- week1/loops_pratice.rb
+++ week1/loops_pratice.rb
@@ -1,13 +1,21 @@
+# # index = 1
+
+# # while index <= 10
+# #   p index
+# #   index += 1
+# # end
+
 # index = 1
 
-# while index <= 10
-#   p index
+# while index <= 5
+#   puts "hello"
 #   index += 1
 # end
 
-index = 1
-
-while index <= 5
-  puts "hello"
-  index += 1
-end
+# while true
+#   puts "Please input a word:"
+#   input = gets.chomp
+#   if input.downcase == "stop"
+#     break
+#   end
+# end
