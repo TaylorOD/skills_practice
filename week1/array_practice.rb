29bad90a22488396f7cff91feaee45bb5f2c0b97
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-28 09:54:20 -0500

    add 2nd array practice

--- week1/array_practice.rb
+++ week1/array_practice.rb
@@ -1,6 +1,12 @@
-three_words = ["Dog", "Cat", "Fish"]
+# three_words = ["Dog", "Cat", "Fish"]
 
-three_words << "Bag"
-three_words << "Loom"
+# three_words << "Bag"
+# three_words << "Loom"
 
-p three_words
+# p three_words
+
+four_array = ["g", "h", "j", "l"]
+
+four_array[1] = 9
+
+p four_array
