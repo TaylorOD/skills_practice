3cb5f6cf101e044629d792ad8bcfd0da174ca843
taylorod <taylorsunwatedemails@gmail.com>
2020-09-24 15:35:38 -0500

    I think i fixed git

--- week_5/loop_within_loop_practice.rb
+++ week_5/loop_within_loop_practice.rb
@@ -141,66 +141,66 @@
 #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 #     For example, [1, 8, 3, 10] becomes 18.
 
-# array_of_numbers = [1, 8, 3, 10]
-# largest_sum = array_of_numbers[0] + array_of_numbers[1]
-# index1 = 0
+array_of_numbers = [1, 8, 3, 10]
+largest_sum = array_of_numbers[0] + array_of_numbers[1]
+index1 = 0
 
-# while index1 < array_of_numbers.length
-#   first_number = array_of_numbers[index1]
-#   index2 = 0
-#   while index2 < array_of_numbers.length
-#     second_number = array_of_numbers[index2]
-#     if index1 != index2
-#       if first_number + second_number > largest_sum
-#         largest_sum = first_number + second_number
-#       end
-#     end
-#     index2 += 1
-#   end
-#   index1 += 1
-# end
+while index1 < array_of_numbers.length
+  first_number = array_of_numbers[index1]
+  index2 = 0
+  while index2 < array_of_numbers.length
+    second_number = array_of_numbers[index2]
+    if index1 != index2
+      if first_number + second_number > largest_sum
+        largest_sum = first_number + second_number
+      end
+    end
+    index2 += 1
+  end
+  index1 += 1
+end
 
-# p largest_sum
+p largest_sum
 
 #  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 #     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
 
-# array_of_numbers = [2, 5, 3, 1, 0, 7, 11]
-# first_pair_to_ten = false
-# index1 = 0
+array_of_numbers = [2, 5, 3, 1, 0, 7, 11]
+first_pair_to_ten = false
+index1 = 0
 
-# while index1 < array_of_numbers.length
-#   first_number = array_of_numbers[index1]
-#   index2 = 0
-#   while index2 < array_of_numbers.length
-#     second_number = array_of_numbers[index2]
-#     if index1 != index2
-#       if first_number + second_number == 10 && first_pair_to_ten == false
-#         first_pair_to_ten = [first_number, second_number]
-#       end
-#     end
-#     index2 += 1
-#   end
-#   index1 += 1
-# end
+while index1 < array_of_numbers.length
+  first_number = array_of_numbers[index1]
+  index2 = 0
+  while index2 < array_of_numbers.length
+    second_number = array_of_numbers[index2]
+    if index1 != index2
+      if first_number + second_number == 10 && first_pair_to_ten == false
+        first_pair_to_ten = [first_number, second_number]
+      end
+    end
+    index2 += 1
+  end
+  index1 += 1
+end
 
-# p first_pair_to_ten
+p first_pair_to_ten
 
 # 10. Use a nested loop to convert an array of string arrays into a single string.
 #     For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
 
-# array_of_strings = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]]
-# single_string = ""
-# index1 = 0
+array_of_strings = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]]
+single_string = ""
+index1 = 0
 
-# while index1 < array_of_strings.length
-#   index2 = 0
-#   while index2 < array_of_strings[index1].length
-#     single_string = single_string + array_of_strings[index1][index2]
+while index1 < array_of_strings.length
+  index2 = 0
+  while index2 < array_of_strings[index1].length
+    single_string = single_string + array_of_strings[index1][index2]
 
-#     index2 += 1
-#   end
-#   index1 += 1
-# end
+    index2 += 1
+  end
+  index1 += 1
+end
 
-# p single_string
+p single_string
