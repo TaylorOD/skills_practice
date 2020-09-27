ceecdcfd16590382165502a81f3b8a742b5d0a26
taylorod <taylorsunwatedemails@gmail.com>
2020-09-26 21:53:34 -0500

    add array practice

---
+++ week_5/arrays_practice.js
@@ -0,0 +1,72 @@
+// # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+// let words = ["bacon", "cookie", "toothpaste"]
+// words.push("fishstick", "water")
+
+// console.log(words)
+
+// # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+// let letters = ["l", "f", "h", "e"]
+
+// letters[1] = 10
+
+// console.log(letters)
+
+// # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+// let numbers = [5, 19, 100, 3, 49]
+// let index = 0
+
+// while (index < numbers.length) {
+//   console.log(numbers[index])
+//   index++
+// }
+
+// # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+// let numbers = [1]
+// numbers.push(11, 6, 100)
+// console.log(numbers)
+
+// # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print that string on a line.
+
+// let lowerCaseLetters = ["hakeem", "taylor", "bruce"]
+// let upperCaseLetters = lowerCaseLetters[2].toUpperCase()
+// console.log(upperCaseLetters)
+
+// # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+// let names = ["Jon", "Sally", "Beth"]
+// let index = 0
+// while (index < names.length) {
+//   console.log(names[index])
+//   index++
+// }
+
+// # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+// let strings = ["word", "leg kick"]
+// strings.unshift("knee strike")
+// console.log(strings)
+
+// # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+// let numbers = [5, 10, 22, 29, 100]
+// numbers[0] = numbers[0] * 10
+// console.log(numbers)
+
+// # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+// let numbers = [19, 100]
+// let index = 0
+// while (index < numbers.length) {
+//   console.log(numbers[index])
+//   index++
+// }
+
+// # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+// let countries = ["Brazil", "USA", "France"]
+// countries.unshift("Germany")
+// console.log(countries)
---
+++ week_5/loops_practice.js
@@ -0,0 +1,101 @@
+// # 1. Write a while loop to print the numbers 1 through 10.
+
+// let index = 1
+
+// while (index <= 10) {
+//   console.log(index)
+//   index++
+// }
+
+// # 2. Write a while loop that prints the word "hello" 5 times.
+
+// let index = 1
+// while (index <= 5) {
+//   console.log("hello")
+//   index++
+// }
+
+// # 3. Write a while true loop that will print the numbers 1 through 100.
+
+// let index = 1
+
+// while (true) {
+//   console.log(index)
+
+//   if (index >= 100) {
+//     break
+//   } else {
+//     index++
+//   }
+// }
+
+// # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+// let index = 0
+
+// while (index <= 100) {
+//   console.log(index)
+//   index += 5
+// }
+
+// # 5. Write a while loop that prints the number 9000 ten times.
+
+// let index = 0
+
+// while (index <= 10) {
+//   console.log(9000)
+//   index++
+// }
+
+// # 6. Write a while true loop that will run forever and print each number until the number greater than 10.
+
+// let index = 0
+
+// while (true) {
+//   if (index > 10) {
+//     break
+//   } else {
+//     console.log(index)
+//     index++
+//   }
+// }
+
+// # 7. Write a while loop that prints the numbers 50 to 70.
+
+// let index = 50
+
+// while (index <= 70) {
+//   console.log(index)
+//   index += 1
+// }
+
+// # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+// let index = 1
+
+// while (index <= 144) {
+//   console.log("Around the world")
+//   index++
+// }
+
+// # 9. Write a while true loop that prints the phrase "cookie" 15 times.
+
+// let index = 1
+
+// while (true) {
+//   if (index <= 15) {
+//     console.log("cookie")
+//     index++
+//   } else {
+//     break
+//   }
+// }
+
+// # 10. Write a while loop that prints the even numbers from 2 to 40.
+
+// let index = 2
+
+// while (index <= 40) {
+//   console.log(index)
+//   index += 2
+// }
