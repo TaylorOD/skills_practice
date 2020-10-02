22452e3441fe301a5e0a69125917b5b46dcc4f1c
taylorod <taylorsunwantedemails@gmail.com>
2020-10-02 12:13:14 -0500

    first 5 solutions

---
+++ week_6/methods_builtin_practice.js
@@ -0,0 +1,66 @@
+// # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+// const wordCaper = function (string) {
+//   return string.toUpperCase()
+// }
+
+// console.log(wordCaper("pizza"))
+
+// # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// const bigNumber = function (number) {
+//   if (number > 100) {
+//     return "That's a big number"
+//   }
+// }
+
+// console.log(bigNumber(101))
+
+// # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+// const numberAdder = function (number1, number2) {
+//   return (number1 + number2)
+// }
+
+// console.log(numberAdder(100, 900))
+
+// # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+// const wordReverser = function (stringArray) {
+//   let splitString = stringArray.split("")
+//   let reverseString = splitString.reverse()
+//   let result = reverseString.join("")
+//   return result
+// }
+
+// console.log(wordReverser("Middle"))
+
+// # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+// const tenTimber = function (number) {
+
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+//   console.log(number)
+
+// }
+
+// tenTimber(100)
+
+// # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+// # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+// # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+// # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+// # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
