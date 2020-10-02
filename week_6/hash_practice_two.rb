e9ef8aead68915b2d726c122d1d96db252b5f145
taylorod <taylorsunwantedemails@gmail.com>
2020-10-02 11:25:25 -0500

    add 5 hash problems

---
+++ week_6/hash_practice_two.rb
@@ -0,0 +1,66 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+# people_info = { first_name: "Taylor", last_name: "Dorsett", email: "td@example.com" }
+# p people_info[:first_name]
+# p people_info[:last_name]
+# p people_info[:email]
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+# peoples_info = [{ first_name: "Tod", last_name: "Dormer" }, { first_name: "Luke", last_name: "Rockhold" }, { first_name: "Diego", last_name: "Schanchez" }]
+# p peoples_info[0]
+
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+# menu_items = {
+#   soda: 3.50,
+#   sub: 5,
+#   hamburger: 6.50,
+# }
+# menu_items["grapes"] = 1.50
+# p menu_items
+
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+# book_info = {
+#   title: "The Stand",
+#   author: "Stephen King",
+#   number_of_pages: 900,
+#   language: "english",
+# }
+
+# p book_info[:title]
+# p book_info[:author]
+# p book_info[:number_of_pages]
+# p book_info[:language]
+
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+# books_info = [{
+
+#   title: "The Shining",
+#   author: "Stephen King",
+
+# }, {
+
+#   title: "The Dead Zone",
+#   author: "Stephen King",
+
+# }, {
+
+#   title: "Misery",
+#   author: "Stephen King",
+
+# }]
+
+# p books_info[2]
+
+# 6. Make a hash to store 3 different states and their capitals. Then add a new state and capital and print the hash to see the result.
+
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+# 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
