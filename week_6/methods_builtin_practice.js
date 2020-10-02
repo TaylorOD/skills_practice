436e405d774ee85b3534346770ae55e4eabcf9d4
taylorod <taylorsunwantedemails@gmail.com>
2020-10-02 12:19:35 -0500

    added built in function solutions

--- week_6/methods_builtin_practice.js
+++ week_6/methods_builtin_practice.js
@@ -57,10 +57,42 @@
 
 // # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
+// const capsPrinter = function (string1, string2) {
+//   return string1.toUpperCase() + " " + string2.toUpperCase()
+// }
+
+// console.log(capsPrinter("Billy", "Bob"))
+
 // # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+// const characterCounter = function (string) {
+//   return string.length
+// }
+
+// console.log(characterCounter("Broken"))
 
 // # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
+// const negativeNumber = function (number) {
+//   if (number < 0) {
+//     return "That's a negative number"
+//   }
+// }
+
+// console.log(negativeNumber(-6))
+
 // # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+// const timesEachOther = function (number1, number2) {
+//   return (number1 * number2)
+// }
+// console.log(timesEachOther(100, 100))
+
 // # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+// const longWord = function (string) {
+//   if (string.length > 5) {
+//     return "That's a long word."
+//   }
+// }
+// console.log(longWord("Prison"))
