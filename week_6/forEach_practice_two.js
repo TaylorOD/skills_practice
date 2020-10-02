8588f8b2109d96ca5fea48a0d3e1f3b05920e12b
taylorod <taylorsunwantedemails@gmail.com>
2020-10-02 15:01:26 -0500

    add 2nd 5 javascirpt for each prctice

--- week_6/forEach_practice_two.js
+++ week_6/forEach_practice_two.js
@@ -57,14 +57,62 @@
 // #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+// let peoplesInfo = [{
+//   name: "Alice",
+//   age: 27
+// }, {
+//   name: "Blane",
+//   age: 16
+// }]
+// let peoplesAges = []
+// peoplesInfo.forEach(function (people) {
+//   peoplesAges.push(people.age)
+// })
+// console.log(peoplesAges)
+
 // #  7. Start with an array of numbers and create a new array with each number divided by 2.
 // #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+// let numbers = [1, 2, 3]
+// let numbersHalfed = []
+// numbers.forEach(function (number) {
+//   numbersHalfed.push(number / 2)
+// })
+// console.log(numbersHalfed)
 
 // #  8. Start with an array of strings and create a new array with each string's first letter only.
 // #     For example, ["hello", "goodbye"] becomes ["h", "g"].
 
+// let strings = ["hello", "goodbye"]
+// let stringsFirst = []
+// strings.forEach(function (string) {
+//   stringsFirst.push(string[0])
+// })
+// console.log(stringsFirst)
+
 // # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+// let peoplesInfo = [{
+//   name: "Alice",
+//   age: 27
+// }, {
+//   name: "Blane",
+//   age: 16
+// }]
+// let peoplesAgesTimesTwo = []
+// peoplesInfo.forEach(function (people) {
+//   peoplesAgesTimesTwo.push(people.age * 2)
+// })
+// console.log(peoplesAgesTimesTwo)
+
 // # 10. Start with an array of numbers and create a new array with each number converted into a string.
 // #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+// let numbers = [1, 2, 3]
+// let numberedStrings = []
+// numbers.forEach(function (number) {
+//   numberedStrings.push(number.toString())
+
+// })
+// console.log(numberedStrings)
