f3380089e3435d8552a53a30fcafc64ce22ba620
taylorod <taylorsunwantedemails@gmail.com>
2020-10-10 23:00:43 -0500

    add 3 forLoop practice questions

--- week_7/forLoop_practice.js
+++ week_7/forLoop_practice.js
@@ -1,16 +1,32 @@
 // write a forloop that counts to 10
-let total = 0
-let limit = 10
+// let total = 0
+// let limit = 10
 
-for (let index = 0; index < limit; index += 1) {
-  total += index
-}
-console.log(total)
+// for (let index = 0; index < limit; index += 1) {
+//   total += index
+// }
+// console.log(total)
 
 // Create a for loop that changes each string in the array so that they are plural.
-let pets = ["cat", "dog", "rat"]
+// let pets = ["cat", "dog", "rat"]
 
-for (let index = 0; index < pets.length; index += 1) {
-  pets[index] = pets[index] + "s"
+// for (let index = 0; index < pets.length; index += 1) {
+//   pets[index] = pets[index] + "s"
+// }
+// console.log(pets)
+
+// Get the sum of two arrays...actually the sum of all their elements.
+
+let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2] // --> 276
+let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26] // --> 351
+let sum1 = 0
+let sum2 = 0
+
+for (var index = 0; index < arr_1.length; index += 1) {
+  sum1 = sum1 + arr_1[index]
 }
-console.log(pets)
+for (var indexTwo = 0; indexTwo < arr_2.length; indexTwo += 1) {
+  sum2 = sum2 + arr_2[indexTwo]
+}
+
+console.log(sum1 + sum2)
