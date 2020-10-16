76bf3e6ca5a50e5913ac3bace9a3b3fd3d4fdebd
taylorod <taylorsunwantedemails@gmail.com>
2020-10-16 12:37:35 -0500

    add 5 select ruby and 5 for loop javascript

--- week_8/nestedLoops.js
+++ week_8/nestedLoops.js
@@ -6,74 +6,26 @@
 //   [8, 9],
 //   [2, 16]
 // ]
-// let index1 = 0
-// let flatArray = []
 
-// while (index1 < numberPairs.length) {
-//   let index2 = 0
-//   let firstLoop = numberPairs[index1]
-//   while (index2 < firstLoop.length) {
-//     flatArray.push(firstLoop[index2])
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(flatArray)
 
 // #  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 // #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
 
 // let lettersOne = ["a", "b", "c"]
 // let lettersTwo = ["d", "e", "f", "g"]
-// let combinedLetters = []
-// let index1 = 0
-// while (index1 < lettersOne.length) {
-//   let index2 = 0
-//   while (index2 < lettersTwo.length) {
-//     combinedLetters.push(lettersOne[index1] + lettersTwo[index2])
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(combinedLetters)
+
 
 // #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 // #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
 // let strings = ["a", "b", "c", "d"]
-// let combinedStrings = []
-// let index1 = 0
-// while (index1 < strings.length) {
-//   let index2 = 0
-//   while (index2 < strings.length) {
-//     if (index1 !== index2) {
-//       combinedStrings.push(strings[index1] + strings[index2])
-//     }
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(combinedStrings)
+
 
 // #  4. Use a nested loop to find the largest product of any two different numbers within a given array.
 // #     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
 
 // let numbers = [5, -2, 1, -9, -7, 2, 6]
-// let largestProduct = numbers[0] * numbers[1]
-// let index1 = 0
-// while (index1 < numbers.length) {
-//   let index2 = 0
-//   while (index2 < numbers.length) {
-//     if (index1 !== index2) {
-//       if (numbers[index1] * numbers[index2] > largestProduct) {
-//         largestProduct = numbers[index1] * numbers[index2]
-//       }
-//     }
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(largestProduct)
+
 
 // #  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
 // #     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
@@ -83,91 +35,33 @@
 //   [8, 9],
 //   [2, 16]
 // ]
-// let result = 0
-// let index1 = 0
-// while (index1 < numberPairs.length) {
-//   let index2 = 0
-//   let currentLoop = numberPairs[index1]
-//   while (index2 < currentLoop.length) {
-//     result = result + currentLoop[index2]
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(result)
+
 
 // #  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
 // #     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
 
 // let numbersOne = [1, 2]
 // let numbersTwo = [6, 7, 8]
-// let result = []
-// let index1 = 0
-// while (index1 < numbersOne.length) {
-//   let index2 = 0
-//   while (index2 < numbersTwo.length) {
-//     result.push(numbersOne[index1] + numbersTwo[index2])
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(result)
+
 
 // #  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 // #     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
 
 // let numbers = [2, 8, 3]
 // let result = []
-// let index1 = 0
-// while (index1 < numbers.length) {
-//   let index2 = 0
-//   while (index2 < numbers.length) {
-//     result.push(numbers[index1] * numbers[index2])
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(result)
+
 
 // #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 // #     For example, [1, 8, 3, 10] becomes 18.
 
 // let numbers = [1, 8, 3, 10]
-// let result = numbers[0] + numbers[1]
-// let index1 = 0
-// while (index1 < numbers.length) {
-//   let index2 = 0
-//   while (index2 < numbers.length) {
-//     if (index1 !== index2) {
-//       if (numbers[index1] + numbers[index2] > result) {
-//         result = numbers[index1] + numbers[index2]
-//       }
-//     }
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(result)
+
 
 // #  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 // #     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
 
 // let numbers = [2, 5, 3, 1, 0, 7, 11]
-// var result = false
-// let index1 = 0
-// while (index1 < numbers.length) {
-//   let index2 = 0
-//   while (index2 < numbers.length) {
-//     if (index1 !== index2) {
-//       if (numbers[index1] + numbers[index2] === 10 && result === false) {
-//         result = [numbers[index1], numbers[index2]]
-//       }
-//     }
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(result)
+
 
 // # 10. Use a nested loop to convert an array of string arrays into a single string.
 // #     For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
@@ -178,15 +72,3 @@
 //   ["a", "canal"],
 //   ["panama"]
 // ]
-// let result = ""
-// let index1 = 0
-// while (index1 < strings.length) {
-//   let index2 = 0
-//   let currentLoop = strings[index1]
-//   while (index2 < currentLoop.length) {
-//     result = result + currentLoop[index2]
-//     index2 += 1
-//   }
-//   index1 += 1
-// }
-// console.log(result)
--- week_8/select_practice.rb
+++ week_8/select_practice.rb
@@ -12,7 +12,7 @@
 
 # words = ["winner", "winner", "chicken", "dinner"]
 # w_words = words.select do |word|
-#   word[0].downcase === "w"
+#   word[0] === "w"
 # end
 # p w_words
 
@@ -20,10 +20,10 @@
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
 # items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
-# items_cost_over_five = items.select do |item|
+# over_five_items = items.select do |item|
 #   item[:price] > 5
 # end
-# p items_cost_over_five
+# p over_five_items
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
@@ -37,53 +37,33 @@
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
-# words = ["a", "man", "a", "plan", "a", "canal", "panama"]
-# words_less_than_four = words.select do |word|
-#   word.length < 4
-# end
-# p words_less_than_four
+words = ["a", "man", "a", "plan", "a", "canal", "panama"]
+under_four = words.select do |word|
+  word.length < 4
+end
+p under_four
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
-# items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
-# items_short_names = items.select do |item|
-#   item[:name].length < 6
-# end
-# p items_short_names
+items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
-# numbers = [8, 23, 0, 44, 1980, 3]
-# less_than_ten = numbers.select do |number|
-#   number < 10
-# end
-# p less_than_ten
+numbers = [8, 23, 0, 44, 1980, 3]
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
-# words = ["big", "little", "good", "bad"]
-# no_b_words = words.select do |word|
-#   word[0] != "b"
-# end
-# p no_b_words
+words = ["big", "little", "good", "bad"]
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
-# items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
-# low_price_items = items.select do |item|
-#   item[:price] < 10
-# end
-# p low_price_items
+items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
-# numbers = [2, 4, 5, 1, 8, 9, 7]
-# odd_numbers = numbers.select do |number|
-#   number % 2 == 1
-# end
-# p odd_numbers
+numbers = [2, 4, 5, 1, 8, 9, 7]
