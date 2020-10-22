a30d662e4f58ef78dd82e2ee21b0d7d1c18564ea
taylorod <taylorsunwantedemails@gmail.com>
2020-10-22 11:49:32 -0500

    daily javascript practice

--- week_9/function_loop_array_practice.js
+++ week_9/function_loop_array_practice.js
@@ -1,83 +1,80 @@
-todo all 10
+// todo all 10
 
 // # 1. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
 // let numbers = [1, 2, 3, 4, 5]
-// let index = 0
-// while (index < numbers.length) {
-//   console.log(numbers[index])
-//   index++
+// let index1 = 0
+// while (index1 < numbers.length) {
+//   console.log(numbers[index1])
+//   index1 += 1
 // }
 
 // # 2. Write a while true loop that will print the numbers 1 through 100.
 
-// let index = 1
-// while (true) {
-//   if (index <= 100) {
-//     console.log(index)
-//     index++
-//   } else {
-//     break
-//   }
+// let index1 = 1
+// while (index1 <= 100) {
+//   console.log(index1)
+//   index1 += 1
 // }
 
 // # 3. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
-// const firstOfString = function (string) {
-//   return string[0]
-// }
-// console.log(firstOfString("knight"))
+// const firstOfString = (string) => string[0]
+// console.log(firstOfString("loser"))
 
 // # 4. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
 // let numbers = [1, 99]
-// let index = 0
-// while (index < numbers.length) {
-//   console.log(numbers[index])
-//   index++
-// }
+// let index1 = 0
+// while (index1 < numbers.length) {
+//   console.log(numbers[index1])
+//   index1 += 1
+// } 
 
 // # 5. Write a while loop that prints the phrase "Around the world" 144 times.
 
-// let index = 1
-// while (index <= 144) {
-//   console.log("Around the world")
-//   index++
+// let index1 = 1
+// while (index1 <= 144) {
+//   console.log("Around the world!")
+//   console.log(index1)
+//   index1 += 1
 // }
 
 // # 6. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
-// const fiveReturn = function (string) {
-//   return string + string + string + string + string
+// let index1 = 0
+// const stringRepeatFiveTimes = (string) => {
+//   while (index1 < 5) {
+//     console.log(string)
+//     index1 += 1
+//   }
 // }
-// console.log(fiveReturn("Push"))
+// stringRepeatFiveTimes("keep")
 
 // # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
 // let strings = ["attack", "roll"]
-// strings.push("dead")
+// strings.push("dive")
 // console.log(strings)
 
 // # 8. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
-// let index = 0
-// while (index <= 100) {
-//   console.log(index)
-//   index += 5
+// let index1 = 0
+// while (index1 <= 100) {
+//   console.log(index1)
+//   index1 += 5
 // }
 
 // # 9. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
 // let names = ["Taylor", "David", "Beverly"]
-// let index = 0
-// while (index < names.length) {
-//   console.log(names[index])
-//   index++
+// let index1 = 0
+// while (index1 < names.length) {
+//   console.log(names[index1])
+//   index1 += 1
 // }
 
 // # 10. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
-// let firstMinusSecond = function (numberOne, numberTwo) {
-//   return numberOne - numberTwo
-// }
-// console.log(firstMinusSecond(100, 70))
+// const subtractedFirstBySecond = (number1, number2) => (number1 - number2)
+// console.log(subtractedFirstBySecond(10, 100))
