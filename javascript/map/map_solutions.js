e2642c3e75ce3e52245f9a10c82db068e11a0526
taylorod <taylorsunwantedemails@gmail.com>
2020-11-25 20:00:29 -0600

    add 10th solution

--- javascript/map/map_solutions.js
+++ javascript/map/map_solutions.js
@@ -117,3 +117,13 @@ console.log(agesTimesTwoLong)
 
 // 10. Start with an array of numbers and create a new array with each number converted into a string.
 // For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+const numberStrings = [1, 2, 3]
+
+const numbersToStrings = numberStrings.map(number => number.toString())
+console.log(numbersToStrings)
+
+const numbersToStringsLong = numberStrings.map(function(number) {
+  return number.toString()
+})
+console.log(numbersToStringsLong)
