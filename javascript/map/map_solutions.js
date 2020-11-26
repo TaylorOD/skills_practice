42270399c6e398432d2b7319728b70b2ba2ad67e
taylorod <taylorsunwantedemails@gmail.com>
2020-11-25 19:48:52 -0600

    add 6th solution

--- javascript/map/map_solutions.js
+++ javascript/map/map_solutions.js
@@ -66,6 +66,16 @@ console.log(stringsArrayLengthLong)
 // 6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 // For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+const peopleInformation = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+
+const peopleAges = peopleInformation.map(person => person.age)
+console.log(peopleAges)
+
+const peopleAgesLong = peopleInformation.map(function(person) {
+  return person.age
+})
+console.log(peopleAgesLong)
+
 // 7. Start with an array of numbers and create a new array with each number divided by 2.
 // For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
