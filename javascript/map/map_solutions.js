bcb291f09705e8c63e845c69586804476c384fe8
taylorod <taylorsunwantedemails@gmail.com>
2020-11-25 19:40:50 -0600

    add 5th solution

--- javascript/map/map_solutions.js
+++ javascript/map/map_solutions.js
@@ -53,6 +53,16 @@ console.log(numbersPlusSevenLong)
 // 5. Start with an array of strings and create a new array with each string's length.
 // For example, ["hello", "goodbye"] becomes [5, 7].
 
+const stringsArray = ["hello", "goodbye"]
+
+const stringsArrayLength = stringsArray.map(string => string.length)
+console.log(stringsArrayLength)
+
+const stringsArrayLengthLong = stringsArray.map(function(string) {
+  return string.length
+})
+console.log(stringsArrayLengthLong)
+
 // 6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 // For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
