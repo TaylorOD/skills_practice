b975940b2b8e5f88e71bf618a9c30a0c86a08861
taylorod <taylorsunwantedemails@gmail.com>
2020-11-26 19:05:23 -0600

    add 2nd for each solution

--- javascript/each_map/each_map_solutions.js
+++ javascript/each_map/each_map_solutions.js
@@ -1,7 +1,7 @@
 // #  1. Start with an array of numbers and create a new array with each number times 3.
 // #     For example, [1, 2, 3] becomes [3, 6, 9]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
 
 let numbers = [1, 2, 3]
 let numbersTimesThree = []
@@ -29,15 +29,26 @@ console.log(numbersTimesThreeMapLong)
 // #  2. Start with an array of strings and create a new array with each string upcased.
 // #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
+let strings = ["hello", "goodbye"]
+let stringsUpcasedForEach = []
 
+strings.forEach(string => stringsUpcasedForEach.push(string.toUpperCase()))
+console.log(stringsUpcasedForEach)
+
+let stringsUpcasedForEachLong = []
+strings.forEach(function(string) {
+  stringsUpcasedForEachLong.push(string.toUpperCase())
+})
+console.log(stringsUpcasedForEachLong)
 
 // .map
+
 
 // #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
 
 
 // .map
@@ -45,7 +56,7 @@ console.log(numbersTimesThreeMapLong)
 // #  4. Start with an array of numbers and create a new array with each number plus 7.
 // #     For example, [1, 2, 3] becomes [8, 9, 10]. Use .forEach and .map.
 
-// ..forEach
+// .forEach
 
 
 // .map
