3e3b34d81c636dc5254544ac8f06cd45ed0b447d
taylorod <taylorsunwantedemails@gmail.com>
2020-12-04 11:24:20 -0600

    add 5th solution

--- javascript/filter/filter_solutions.js
+++ javascript/filter/filter_solutions.js
@@ -40,18 +40,28 @@
 // #  4. Start with an array of numbers and create a new array with only the even numbers.
 // #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
-let numbers = [2, 4, 5, 1, 8, 9, 7]
+// let numbers = [2, 4, 5, 1, 8, 9, 7]
 
-const evenNumbers = numbers.filter(number => number % 2 === 0)
-console.log(evenNumbers)
+// const evenNumbers = numbers.filter(number => number % 2 === 0)
+// console.log(evenNumbers)
 
-const evenNumbersLong = numbers.filter(function (number) {
-  return number % 2 === 0
-})
-console.log(evenNumbersLong)
+// const evenNumbersLong = numbers.filter(function (number) {
+//   return number % 2 === 0
+// })
+// console.log(evenNumbersLong)
 
 // #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 // #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+let strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+
+const stringsUnderFour = strings.filter(string => string.length < 4)
+console.log(stringsUnderFour)
+
+const stringsUnderFourLong = strings.filter(function (string) {
+  return string.length < 4
+})
+console.log(stringsUnderFourLong)
 
 // #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
