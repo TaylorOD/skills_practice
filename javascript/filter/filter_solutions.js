7354fee348de70d9e51f5b3edec76aef62db6367
taylorod <taylorsunwantedemails@gmail.com>
2020-12-04 11:29:09 -0600

    add 6th solution

--- javascript/filter/filter_solutions.js
+++ javascript/filter/filter_solutions.js
@@ -53,18 +53,28 @@
 // #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 // #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
-let strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+// let strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
 
-const stringsUnderFour = strings.filter(string => string.length < 4)
-console.log(stringsUnderFour)
+// const stringsUnderFour = strings.filter(string => string.length < 4)
+// console.log(stringsUnderFour)
 
-const stringsUnderFourLong = strings.filter(function (string) {
-  return string.length < 4
-})
-console.log(stringsUnderFourLong)
+// const stringsUnderFourLong = strings.filter(function (string) {
+//   return string.length < 4
+// })
+// console.log(stringsUnderFourLong)
 
 // #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+let itemsInfo = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+
+const itemNameLessThanSix = itemsInfo.filter(item => item.name.length < 6)
+console.log(itemNameLessThanSix)
+
+const itemNameLessThanSixLong = itemsInfo.filter(function (item) {
+  return item.name.length < 6
+})
+console.log(itemNameLessThanSixLong)
 
 // #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 // #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
