3d957d3e84d86799123eefe911602347e4f0a44c
taylorod <taylorsunwantedemails@gmail.com>
2020-12-06 11:40:57 -0600

    add 3rd solution

--- javascript/reduce/reduce_solutions.js
+++ javascript/reduce/reduce_solutions.js
@@ -3,29 +3,39 @@
 
 // let numbers = [5, 10, 8, 3]
 
-// const sumNumbers = numbers.reduce((sum, number) => sum + number )
+// const sumNumbers = numbers.reduce(((sum, number) => sum + number), 0)
 // console.log(sumNumbers)
 
 // const sumNumbersLong = numbers.reduce(function (sum, number) {
 //   return sum + number
-// })
+// }, 0)
 // console.log(sumNumbersLong)
 
 // #  2. Start with an array of strings and combine them all into a single string.
 // #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
 
-let strings = ["volleyball", "basketball", "badminton"]
+// let strings = ["volleyball", "basketball", "badminton"]
 
-const stringsCombined = strings.reduce((combined_string, string) => combined_string + string)
-console.log(stringsCombined)
+// const stringsCombined = strings.reduce(((combined_string, string) => combined_string + string), "")
+// console.log(stringsCombined)
 
-const stringsCombinedLong = strings.reduce(function (combined_string, string) {
-  return combined_string + string
-})
-console.log(stringsCombinedLong)
+// const stringsCombinedLong = strings.reduce(function (combined_string, string) {
+//   return combined_string + string
+// }, "")
+// console.log(stringsCombinedLong)
 
 // #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+let itemsInfo = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+
+const itemPriceSum = itemsInfo.reduce((sum, item) => (sum + item.price), 0)
+console.log(itemPriceSum)
+
+const itemPriceSumLong = itemsInfo.reduce(function (sum, item) {
+  return sum + item.price
+}, 0)
+console.log(itemPriceSumLong)
 
 // #  4. Start with an array of numbers and compute the the minimum number.
 // #     For example, [5, 10, 8, 3, 9] becomes 3.
