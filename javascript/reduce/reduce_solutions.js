15ec5411344752839c755b0e9670a9a44f58f56a
taylorod <taylorsunwantedemails@gmail.com>
2020-12-07 14:42:54 -0600

    add 6th solution

--- javascript/reduce/reduce_solutions.js
+++ javascript/reduce/reduce_solutions.js
@@ -40,32 +40,43 @@
 // #  4. Start with an array of numbers and compute the the minimum number.
 // #     For example, [5, 10, 8, 3, 9] becomes 3.
 
-let numbers = [5, 10, 8, 3, 9]
+// let numbers = [5, 10, 8, 3, 9]
 
-const minimumNumber = numbers.reduce(function (minNumber, number) {
-  if (number < minNumber) {
-    return number
-  } else {
-    return minNumber
-  }
-}, numbers[0])
-console.log(minimumNumber)
+// const minimumNumber = numbers.reduce(function (minNumber, number) {
+//   if (number < minNumber) {
+//     return number
+//   } else {
+//     return minNumber
+//   }
+// }, numbers[0])
+// console.log(minimumNumber)
 
 // #  5. Start with an array of strings and compute the total length of all the strings.
 // #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
 
-let strings = ["volleyball", "basketball", "badminton"]
+// let strings = ["volleyball", "basketball", "badminton"]
 
-const stringsLength = strings.reduce((length, string) => (length + string.length), 0)
-console.log(stringsLength)
+// const stringsLength = strings.reduce((length, string) => (length + string.length), 0)
+// console.log(stringsLength)
 
-const stringsLengthLong = strings.reduce(function (length, string) {
-  return length + string.length
-}, 0)
-console.log(stringsLengthLong)
+// const stringsLengthLong = strings.reduce(function (length, string) {
+//   return length + string.length
+// }, 0)
+// console.log(stringsLengthLong)
 
 // #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+let itemsInfo = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+
+const lowestPriceItem = itemsInfo.reduce(function (lowestPrice, item) {
+  if (item.price < lowestPrice) {
+    return item
+  } else {
+    return lowestPrice
+  }
+}, itemsInfo[0].price)
+console.log(lowestPriceItem)
 
 // #  7. Start with an array of numbers and compute product of all the numbers.
 // #     For example, [5, 10, 8, 3] becomes 1200.
